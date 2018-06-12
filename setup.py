07d720aff4115f29344259edda43696fdd625822
H. Chase Stevens <chase@chasestevens.com>
2018-06-11 21:03:37 -0400

    Bump version to 0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='fault-localization',
     packages=['fault_localization'],
     platforms='any',
-    version='0.0.1',
+    version='0.1.0',
     description='A fault localization tool for Python\'s pytest testing framework.',
     author='H. Chase Stevens',
     author_email='chase@chasestevens.com',
